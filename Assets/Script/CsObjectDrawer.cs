315590f9ba4f3186ae659ac23e73483bc17bb7f4
KimHassan <38421611+KimHassan@users.noreply.github.com>
2019-05-19 22:36:16 +0900

    머지?

---
+++ Assets/Script/CsObjectDrawer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CsObjectDrawer : MonoBehaviour
+{
+    Vector3 originPos;
+
+    Vector3 destPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originPos = transform.localPosition;
+
+        destPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
