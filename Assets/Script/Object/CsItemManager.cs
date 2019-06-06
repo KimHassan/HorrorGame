50085d8651ab6e5b597dcf3077621465a41cdb89
KimHassan <38421611+KimHassan@users.noreply.github.com>
2019-06-06 11:28:46 +0900

    가구들 model하고 prefab 폴더 정리, 라이트 분리된거 추가, 맵 배치 약간 변경, 아이템 나오는 임의 좌표 가구들에 추가, 게임 시작시 아이템 랜덤 생성(약통)

--- Assets/Script/Object/CsItemManager.cs
+++ Assets/Script/Object/CsItemManager.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CsItemManager : MonoBehaviour
 {
-    GameObject[] activeItem = new GameObject[10];
+    GameObject[] itemPosition;
 
-    Vector3[] itemPosition = new Vector3[20];
+    public GameObject drugItem;
 
+    
+
+    //public GameObject[] keyItem;
+
+    int max;
+
+    int[] arrRandPosition; // 아이템을 배치할 랜덤의 수
+
+    List<int> arrRand = new List<int>();
+
+
+    int itemMax = 5;
 
     // Start is called before the first frame update
     void Start()
     {
+       itemPosition = GameObject.FindGameObjectsWithTag("ItemPosition");
+
+        max = itemPosition.Length;
+
         
+        arrRandPosition = new int[itemMax];
+
+        setItems();
     }
 
     // Update is called once per frame
@@ -23,6 +43,45 @@ public class CsItemManager : MonoBehaviour
 
     void setItems()
     {
+        for (int i = 0; i < itemMax; i++)
+        {
+            arrRandPosition[i] = Random.Range(0, max);
+        }
+        for (int i = 0; i < itemMax; i++)
+        {
+            arrRand.Add(Random.Range(0, max));
+        }
+     
+
+        arrRand.Sort();
+
+
+        Debug.Log(arrRand.Count);
+
+
+        for (int i = 0; i < itemPosition.Length; i++)
+        {
+            if(arrRand.Count != 0)
+            {
+                if (i == arrRand[0])
+                {
+
+                    GameObject drug = Instantiate(drugItem, itemPosition[i].transform.position, Quaternion.identity);
+
+                    drug.transform.localRotation = drugItem.transform.localRotation;
+
+                    drug.transform.parent = itemPosition[i].transform.parent;
+
+                    arrRand.RemoveAt(0);
+
+                }
+            }
+            
+
+            Destroy(itemPosition[i]);
+            
+        }
+
 
     }
 }
