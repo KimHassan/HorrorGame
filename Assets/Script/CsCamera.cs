d30e5d2eba16db573a81610f33c57a1bff0e108e
LeeYonggi <rl4459@naver.com>
2019-07-01 16:50:35 +0900

    용기는 마지막입니다

--- Assets/Script/CsCamera.cs
+++ Assets/Script/CsCamera.cs
@@ -102,6 +102,6 @@ public class CsCamera : MonoBehaviour
 
     void TextUp()
     {
-        CsUIControll.instance.ChangeText("뭔가 이상하다. 이곳에서 나가자");
+        CsUIControll.instance.ChangeText("뭔가 이상하다. 이 집에서 나가자");
     }
 }
